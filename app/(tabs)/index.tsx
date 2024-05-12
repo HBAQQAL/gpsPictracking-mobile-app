fb65333e1d5ca29d0c0c732857df12a3ba7b1a06
HBAQQAL <hamzabaqqal2002@gmail.com>
2024-05-12 17:06:19 +0100

    adding file structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { Link } from "expo-router";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import * as Camera from "expo-camera";
+
+async function getCameraPermission() {}
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Capture the nature</Text>
+      <Button title="Take a picture" onPress={getCameraPermission} />
     </View>
   );
 }
@@ -16,16 +21,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
---
+++ app/auth/_layout.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { Link, Tabs } from "expo-router";
+import React, { Component } from "react";
+import { Stack } from "expo-router/stack";
+
+export class index extends Component {
+  render() {
+    return <Stack />;
+  }
+}
+
+export default index;
